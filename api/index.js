479e30005dbcbec116a7eaae77c8f211ff4d706f
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-19 12:24:12 -0500

    controllers_and_routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -9,4 +11,19 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(3000, () => console.log('Listening on port 3000'))
+
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
+
+app.use((err, req, res, next) => {
+    const statuscode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statuscode).json({
+        success: false,
+        statuscode,
+        message
+    })
+})
