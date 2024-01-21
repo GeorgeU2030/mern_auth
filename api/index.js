04db76d776a9816c499af039a84a6177355b24f6
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-20 19:31:27 -0500

    new_cors_config

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(
     cors({
       credentials:true,
-      origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+      origin: process.env.FRONTEND_URL ,
       optionsSuccessStatus: 200,
     }),
 );
--- clientapp/src/pages/SignIn.jsx
+++ clientapp/src/pages/SignIn.jsx
@@ -25,6 +25,7 @@ export default function SignIn() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify(formData),
       });
