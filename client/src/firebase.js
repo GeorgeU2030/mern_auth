560253e6de70648f54fc80028c4840745874f87b
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-20 22:20:58 -0500

    firebase_configuration

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-auth-1c4ae.firebaseapp.com',
-  projectId: 'mern-auth-1c4ae',
-  storageBucket: 'mern-auth-1c4ae.appspot.com',
-  messagingSenderId: '277641423672',
-  appId: '1:277641423672:web:2de25252aae022d51aafcd',
+  authDomain: "mernauth-c5879.firebaseapp.com",
+  projectId: "mernauth-c5879",
+  storageBucket: "mernauth-c5879.appspot.com",
+  messagingSenderId: "393143723543",
+  appId: "1:393143723543:web:b91e1ec50c357f1bef5e52"
 };
 
 // Initialize Firebase
